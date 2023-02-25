a8433580dd2787414ef2497492bad434c6f76f96
prabhu <prabhu_tc@yahoo.com>
2023-02-25 10:20:10 +0530

    [Add/update files]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavigationMenu from './components/NavigationMenu';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <NavigationMenu />
-      {/* <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div> */}
+      <BrowserRouter>
+        <NavigationMenu />
+
+        <Container>
+          <Routes>
+            <Route path="/about" element={<h1>Hi About</h1>} />
+            <Route path="/restaurants/:id" element={<h1>Hi restaurant details</h1>} />
+            <Route path="/restaurants" element={<h1>Hi restaurants List</h1>} />
+            <Route path="/" element={<h1>Hi Home</h1>} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/components/NavigationMenu.tsx
+++ client/src/components/NavigationMenu.tsx
@@ -1,9 +1,6 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -12,7 +9,7 @@ const NavigationMenu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Brand to="/" as={NavLink}>Navbar scroll</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -20,21 +17,8 @@ const NavigationMenu = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
-                        </Nav.Link>
+                        <Nav.Link to="/restaurants" as={NavLink}>restaurants</Nav.Link>
+                        <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
                     </Nav>
                     <Form className="d-flex">
                         <Form.Control
