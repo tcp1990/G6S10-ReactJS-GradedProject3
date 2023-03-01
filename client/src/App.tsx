7388e1e42b59ceaa3531c6a14382264a3f54b1ce
prabhu <prabhu_tc@yahoo.com>
2023-03-01 10:42:06 +0530

    [Add/update files]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import NavigationMenu from './components/navbar/NavigationMenu';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import IMovieItem from "./models/IMovieItem";
 import Home from './components/moviepages/Home';
 import ComingSoon from './components/moviepages/ComingSoon';
 import TopRatedIndian from './components/moviepages/TopRatedIndian';
@@ -12,38 +10,6 @@ import TopRatedMovies from './components/moviepages/TopRatedMovies';
 import MovieDetails from './components/MovieDetails';
 
 function App() {
-
-	const [favourites, setFavourites] = useState<IMovieItem[]>([]);
-
-	useEffect(() => {
-		const movieFavourites = JSON.parse(
-			localStorage.getItem('react-movie-app-favourites') || '{}'
-		);
-
-		if (movieFavourites) {
-			setFavourites(movieFavourites);
-		}
-	}, []);
-
-	const saveToLocalStorage = (items: IMovieItem[]) => {
-		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
-	};
-
-	const addFavouriteMovie = (movie: IMovieItem) => {
-		const newFavouriteList = [...favourites, movie];
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
-
-	const removeFavouriteMovie = (movie: IMovieItem) => {
-		const newFavouriteList = favourites.filter(
-			(favourite) => favourite.id !== movie.id
-		);
-
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
-
 
 	return (
 		<>
@@ -54,10 +20,10 @@ function App() {
 					<Routes>
 						<Route path="/movies/:id" element={<MovieDetails />} />
 						<Route path="/favourities" element={<Favourites />} />
-						<Route path="/top-rated-movies" element={<TopRatedMovies handleFavouritesClick={addFavouriteMovie} />} />
-						<Route path="/top-rated-indian" element={<TopRatedIndian handleFavouritesClick={addFavouriteMovie} />} />
-						<Route path="/coming-soon" element={<ComingSoon handleFavouritesClick={addFavouriteMovie} />} />
-						<Route path="/" element={<Home handleFavouritesClick={addFavouriteMovie} />} />
+						<Route path="/top-rated-movies" element={<TopRatedMovies />} />
+						<Route path="/top-rated-indian" element={<TopRatedIndian />} />
+						<Route path="/coming-soon" element={<ComingSoon />} />
+						<Route path="/" element={<Home />} />
 					</Routes>
 				</Container>
 			</BrowserRouter>
