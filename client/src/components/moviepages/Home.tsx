fbcac247e2a40f1dbd0e319cf4407a58222337f7
prabhu <prabhu_tc@yahoo.com>
2023-03-01 13:01:39 +0530

    [Add/update files]

--- client/src/components/moviepages/Home.tsx
+++ client/src/components/moviepages/Home.tsx
@@ -1,13 +1,23 @@
 import { useState, useEffect } from 'react';
 import IMovieItem from "../../models/IMovieItem";
-import { getMoviesInTheaters } from '../../services/movies';
+import { addFavourites, getMoviesInTheaters } from '../../services/movies';
 import MovieCardItem from '../MovieCardItem';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, ToastContainer, Toast } from 'react-bootstrap';
 import FavouriteComponent from '../favourites/AddFavourites';
-import { addFavouriteMovie } from '../common';
+
+type State = {
+    responseState: 'initial' | 'success' | 'error',
+    toastMessage: string,
+    show: boolean
+};
 
 const Home = (props: any) => {
     const [movies, setMovies] = useState<IMovieItem[]>([]);
+    const [state, setState] = useState<State>({
+        responseState: 'initial',
+        toastMessage: '',
+        show: false
+    });
 
     useEffect(() => {
         const getMovies = async () => {
@@ -24,6 +34,30 @@ const Home = (props: any) => {
         getMovies();
     }, []);
 
+    const addFavouriteMovieAction = async (movie: IMovieItem) => {
+        try {
+            setState({
+                ...state,
+                responseState: 'initial'
+            });
+            const data = await addFavourites(movie);
+            setState({
+                ...state,
+                responseState: 'success',
+                toastMessage: `A menu item with id=${data.id} has been added successfully`,
+                show: true
+            });
+
+        } catch (error) {
+            setState({
+                ...state,
+                responseState: 'error',
+                toastMessage: (error as Error).message,
+                show: true
+            });
+        }
+    }
+
     return (
         <>
             <Row xs={1} md={2} lg={3}>
@@ -33,12 +67,32 @@ const Home = (props: any) => {
                             <MovieCardItem
                                 movie={movie}
                                 favouriteComponent={<FavouriteComponent />}
-                                handleFavouritesClick={addFavouriteMovie}
+                                handleFavouritesClick={addFavouriteMovieAction}
                             />
                         </Col>
                     ))
                 }
             </Row>
+            {
+                state.responseState !== 'initial' && (
+                    <ToastContainer className="p-3" position="top-end">
+                        <Toast
+                            bg={state.responseState === 'success' ? 'success' : 'danger'}
+                            show={state.show}
+                            autohide
+                            delay={5000}
+                            onClose={() => setState({ ...state, show: false })}
+                        >
+                            <Toast.Header closeButton={false}>
+                                {state.responseState === 'success' ? 'Success' : 'Error'}
+                            </Toast.Header>
+                            <Toast.Body>
+                                {state.toastMessage}
+                            </Toast.Body>
+                        </Toast>
+                    </ToastContainer>
+                )
+            }
         </>
     );
 };
